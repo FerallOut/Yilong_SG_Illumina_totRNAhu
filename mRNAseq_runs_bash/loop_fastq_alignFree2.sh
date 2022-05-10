31fc7cf787560c612fc41099eb9370a2fa6ba90d
FerallOut <49016186+FerallOut@users.noreply.github.com>
2022-05-10 10:28:00 +0200

    bash scripts to run the mRNA-seq pipeline

---
+++ mRNAseq_runs_bash/loop_fastq_alignFree2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#module load snakePipes
+
+for k in $(find /data/processing1/bioinfo_core/requests/FANCI_RNA_seq_4stresses/sampleSheets/new_ss -iname 'sgRNA_*' -printf '%P\n')
+do
+    mRNA-seq -i /data/processing1/bioinfo_core/requests/FANCI_RNA_seq_4stresses/fastq/ \
+    -o /data/processing1/bioinfo_core/requests/FANCI_RNA_seq_4stresses/analysis_fastq_free_trim_gtffilter/ \
+    -v -j 20 --DAG --emailAddress balan@ie-freiburg.mpg.de --smtpServer mail.ie-freiburg.mpg.de \
+    --emailSender balan@ie-freiburg.mpg.de --trim -m alignment-free,deepTools_qc \
+    --filterGTF='-v pseudogene' --sampleSheet ./sampleSheets/new_ss/${k} --rMats hg38
+done
+
