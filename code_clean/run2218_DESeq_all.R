ac240274ac8d4402b040e900e26c5633e5d0bb37
FerallOut <49016186+FerallOut@users.noreply.github.com>
2023-08-16 22:35:44 +0200

    clean up the code for the plots that are not needed anymore

---
+++ code_clean/run2218_DESeq_all.R
@@ -0,0 +1,290 @@
+---
+title: "2218_Yilong_mRNA_DESeq2"
+output:
+  html_document:
+    df_print: paged
+toc: yes
+---
+
+# 1. Sample sheet
+
+name    batch   condition       group
+#################################################
+s1      collA   totRNA_mock     All
+s2      collA   totRNA_mock     All
+s3      collB   totRNA_mock     All
+#################################################
+s4      collA   totRNA_arsenite treatment1
+s5      collB   totRNA_arsenite treatment1
+s6      collB   totRNA_arsenite treatment1
+s7      collB   totRNA_HS       treatment2
+s8      collB   totRNA_HS       treatment2
+s9      collB   totRNA_HS       treatment2
+s10     collA   totRNA_NaCl     treatment3
+s11     collB   totRNA_NaCl     treatment3
+s12     collB   totRNA_NaCl     treatment3
+s13     collA   totRNA_UV       treatment4
+s14     collA   totRNA_UV       treatment4
+s15     collB   totRNA_UV       treatment4
+##################################################
+s16     collA   sgRNA_arsenite  treatment1
+s17     collB   sgRNA_arsenite  treatment1
+s18     collB   sgRNA_arsenite  treatment1
+s19     collB   sgRNA_HS        treatment2
+s20     collB   sgRNA_HS        treatment2
+s21     collB   sgRNA_HS        treatment2
+s22     collA   sgRNA_NaCl      treatment3
+s23     collB   sgRNA_NaCl      treatment3
+s24     collB   sgRNA_NaCl      treatment3
+s25     collA   sgRNA_UV        treatment4
+s26     collA   sgRNA_UV        treatment4
+s27     collB   sgRNA_UV        treatment4
+
+
+# 2. Load libraries and default parameters
+```{r, load-libraries, include=FALSE}
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(DESeq2))
+suppressPackageStartupMessages(library(gtools))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(grid))
+#suppressPackageStartupMessages(library(apeglm))
+#suppressPackageStartupMessages(library(ashr))
+#suppressPackageStartupMessages(library(pheatmap))
+#suppressPackageStartupMessages(library(tibble))
+#library(RColorBrewer)
+.pardefault <- par()
+```
+
+# 3. Load and clean the necessary data 
+
+## 3.a) Load the data
+```{r, load_data}
+dfolder <- "/data/manke/group/balan/projects/Yilong_SG_Illumina_totRNAhu/output/"
+fcounts_gene <- "1a_mRNAseq_alignment_1SampleSheet/featureCounts/counts.tsv"
+
+df.counts_gene <- read.delim(paste0(dfolder, fcounts_gene), header = TRUE, sep='\t', na.strings="NA", row.names = 1) 
+df.counts_gene  %>% head()
+```
+
+There are `r dim(dfcounts_gene)[1]` genes/ rows and `r dim(dfcounts_gene)[2]` samples/ columns in the gene count data frame.
+
+
+## 3.b) Load the metadata
+```{r, label=check_counts, echo=FALSE}
+fmeta_all <- "/data/manke/group/balan/projects/Yilong_SG_Illumina_totRNAhu/data_int/sampleSheets/all_batch_corrected_sampleSheet.tsv"
+df.meta_all <- read.delim(fmeta_all, header = TRUE, sep='\t', na.strings="NA", row.names = 1)
+df.meta_all  %>% head()
+```
+
+## 3.c) Rearrange, clean and factor the data
+
+There are `r dim(dfcounts_gene)[1]` genes in total.
+
+```{r, label=cleanup}
+# cleanup the metadata 
+df.meta_all$condition <- gsub('arsenite', 'ars', df.meta_all$condition) 
+#####################
+
+# cleanup the data
+# reorder the columns alphanumerically
+df.counts_gene <- df.counts_gene[gtools::mixedorder(colnames(df.counts_gene))]
+
+# remove the all the '_PAR_Y' rows because are empty
+rows_PAR_Y <- row.names(df.counts_gene[grepl(pattern="_PAR_Y", row.names(df.counts_gene) ),])
+df.counts_gene <- df.counts_gene[!row.names(df.counts_gene) %in% rows_PAR_Y,]
+
+# remove the version number
+row.names(df.counts_gene) <- gsub("\\..*","", row.names(df.counts_gene))
+# are the metadata rows and data columns in the same order?
+all( df.meta_all$name == colnames(df.counts_gene))
+
+df.counts_gene  %>% head()
+df.meta_all  %>% head()
+```
+
+After eliminating the PAR_Y genes, now there are still `r dim(dfcounts_gene)[1]` genes left.
+
+```{r, factoring}
+# populate the columns 'condition', 'batch' and 'group' with factors
+df.meta_all[c("batch", "condition", "group")] <- lapply(df.meta_all[c("batch", "condition", "group")], factor)
+
+#rearrange sample order:
+l.sample_order <- c('totRNA_mock', 'totRNA_ars', 'totRNA_HS', 'totRNA_NaCl', 'totRNA_UV', 'sgRNA_ars', 'sgRNA_HS', 'sgRNA_NaCl', 'sgRNA_UV')
+df.meta_all$condition <- factor(df.meta_all$condition, levels = l.sample_order)
+
+df.meta_all  %>% head()
+#df.meta_all$condition
+```
+
+
+## 4) Prepare to run DESeq2
+
+```{r, design}
+# create design
+design <- ~condition + batch
+
+# make unfiltered dds obj
+dds <- DESeq2::DESeqDataSetFromMatrix(countData = df.counts_gene, colData = df.meta_all, design = design)
+
+# transform the dds object for plotting 
+rld <- rlog(dds, blind = TRUE)
+```
+
+
+### 4)a. PCA plots batch
+
+#### 4)a.1. PCA plots, one at a time
+```{r, plot_PCA_batch}
+# add a manual color palette (9-colors)
+# attach specific colors for each level of the condition column, so that for the subsets can retain the color scheme
+l.colors <- c("dark grey",
+              "royalblue","#00b159", "#ffc425", "red",
+              "#00aedb", "darkseagreen1", "yellow", "pink")
+palette.color <-setNames(l.colors, levels(rld$condition))
+####################
+
+# 1) for all samples:
+rld.PCA_all <- plotPCA(rld, intgroup=c("batch", "condition"), returnData=TRUE)
+
+percentVar <- round(100 * attr(rld.PCA_all, "percentVar"), 1)   # rounded values for each axis of PC variance
+
+
+# 2) for samples with input from whole samples (totRNA)
+v.cond_totRNA <- rld$condition[grepl(pattern = "totRNA_*", rld$condition), drop = TRUE]
+rld.sub_totRNA <- rld[,rld$condition %in% v.cond_totRNA]
+#rld.sub_totRNA$condition <- droplevels(rld.sub_totRNA$condition)
+
+# choose colors:
+sub.palette.color  <- palette.color[names(palette.color) %in% levels(v.cond_totRNA)]
+
+rld.PCA_totRNA <- plotPCA(rld.sub_totRNA, intgroup=c("batch", "condition"), returnData=TRUE)
+
+percentVar <- round(100 * attr(rld.PCA_totRNA, "percentVar"), 1)   # rounded values for each axis of PC variance
+
+# 3) for samples with input from stress granules (sgRNA)
+v.cond_sgRNA <- rld$condition[grepl(pattern = "sgRNA_*", rld$condition), drop=TRUE]
+rld.sub_sgRNA <- rld[,rld$condition %in% v.cond_sgRNA]
+#rld.sub_sgRNA$condition <- droplevels(rld.sub_sgRNA$condition)
+
+# choose colors:
+sub.palette.color  <- palette.color[names(palette.color) %in% levels(v.cond_sgRNA)]
+
+rld.PCA_sgRNA <- DESeq2::plotPCA(rld.sub_sgRNA, intgroup=c("batch", "condition"), returnData=TRUE)
+
+percentVar <- round(100 * attr(rld.PCA_sgRNA, "percentVar"), 1)   # rounded values for each axis of PC variance
+```
+
+#### 4)a.2. PCA plots, one at a time from a loop
+
+```{r, PCA_loop_singles}
+out_dir <- "/data/manke/group/balan/projects/Yilong_SG_Illumina_totRNAhu/output/2a_mRNAseq_DESeq_manual"
+ 
+par(oma = c(3,4,4,1),    # margins for whole plot
+    mar = c(2,3,2.2,2),  # margins for individual plots
+    mfrow = c(1, 3) )
+#####################################
+ 
+l.colors <- c("dark grey",
+              "royalblue","#00b159", "#ffc425", "red",
+              "#00aedb", "darkseagreen1", "yellow", "pink")
+# set the levels as names for colors for the rld dataset, such that the other 2 being subsets, will inherit the same colors
+palette.color <-setNames(l.colors, levels(rld$condition))
+######################################
+
+l.namePCA2 <- list("rld", "rld.sub_sgRNA", "rld.sub_totRNA")
+l.plots <- list()
+
+# run function and save plots to a list, use lines 'p<-' and 'l.plots'
+# to just show the plots (comment previous lines; uncomment 'print' line)
+for (i in 1:length(l.namePCA2) ) {
+  p <- f.PCAreplot2(get(l.namePCA2[[i]])) + 
+    # add titles for each plot
+    ggtitle(paste0("PCA plot for ", l.namePCA2[i]) )  + 
+    theme(plot.title = element_text(hjust = 0.5)) +
+    # drop the legends
+    theme(legend.position = "none") 
+  
+  l.plots[[i]] = p
+  #print(f.PCAreplot(get(l.namePCA2[[i]])))
+}
+
+########## to arrange the legend in the middle #############
+
+grid_arrange_shared_legend <- function(..., ncol = length(list(...)), nrow = 1, position = c("bottom", "right")) {
+  plots <- list(...)
+  position <- match.arg(position)
+  g <- ggplotGrob(plots[[1]] +
+  #    theme(legend.position = position))$grobs
+      theme(legend.position="bottom", legend.box="vertical") )$grobs
+  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+  lheight <- sum(legend$height)
+  lwidth <- sum(legend$width)
+  gl <- lapply(plots, function(x) x +
+      theme(legend.position = "none"))
+  gl <- c(gl, ncol = ncol, nrow = nrow)
+
+  combined <- switch(position,
+    "bottom" = arrangeGrob(do.call(arrangeGrob, gl),
+      legend,ncol = 1,
+      heights = unit.c(unit(1, "npc") - lheight, lheight)),
+    "right" = arrangeGrob(do.call(arrangeGrob, gl),
+      legend, ncol = 2,
+      widths = unit.c(unit(1, "npc") - lwidth, lwidth)))
+
+  grid.newpage()
+  grid.draw(combined)
+
+  # return gtable invisibly
+  invisible(combined)
+}
+
+q<-grid_arrange_shared_legend(l.plots[[1]], l.plots[[2]], l.plots[[3]], ncol = 3)
+ggsave(filename=paste0("overlap_noBatch.png"), device ="png", path=out_dir, dpi=300, width = 16, height = 5, q) 
+#################################
+```
+
+
+## 5) Run DESeq and get results
+
+```{r, run-DESeq2}
+des <- DESeq(dds) 
+res <- results(des)
+```
+
+
+```{r, MAplots}
+png("./MA_plot2.png",
+  width = 8*300,        # 5 x 300 pixels
+  height = 5*300,
+  res = 300,            # 300 pixels per inch
+  pointsize = 8)        # smaller font size
+
+
+.pardefault <- par()
+
+par(oma = c(3,4,4,1),    # margins for whole plot
+    mar = c(3,4,3.2,1)  # margins for individual plots
+    ) 
+
+yl <- c(-6,6)
+  
+layout(matrix(c(2, 3, 6, 4, 5, 1), 2, 3, byrow=TRUE))
+layout.show(6)
+options(scipen=2) # to get scientific notation; the higher, the less the probability of lossing scientific notation
+
+for (i in 1:length(l.objects) ) {
+  DESeq2::plotMA(Rdf[[i]][[2]], ylim=yl, alpha=metadata(Rdf[[i]][[2]])$alpha/5, main=l.objects[[i]] , xlab='', ylab='', colSig='red',
+                 cex.axis=1.5, cex.main=1.5)
+  abline(h=c(-1,1),col="dodgerblue",lwd=2)
+  }
+
+title(main = paste0("MA plots for DE analysis, alpha=", metadata(Rdf[[i]][[2]])$alpha/5),
+      xlab = "Mean expression",
+      ylab = "Log fold change",
+      outer = TRUE, line = 1,
+      cex.lab=2, cex.main=2)
+
+par(.pardefault)
+dev.off()
+```
